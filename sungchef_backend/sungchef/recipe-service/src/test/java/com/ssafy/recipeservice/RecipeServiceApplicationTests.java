da259c80e466bec2432097ebf781afe8d10712b1
ksh7412 <ksh7412.dev@gmail.com>
2024-03-14 16:01:43 +0900

    init: 프로젝트 초기 환경 세팅

---
+++ sungchef_backend/sungchef/recipe-service/src/test/java/com/ssafy/recipeservice/RecipeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.recipeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sungchef_backend/sungchef/recommend-service/src/main/java/com/ssafy/recommendservice/RecommendServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ssafy.recommendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RecommendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendServiceApplication.class, args);
+	}
+
+}
