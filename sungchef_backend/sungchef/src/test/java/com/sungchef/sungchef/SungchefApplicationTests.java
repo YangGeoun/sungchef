360fc17b6580adfd19d0e84c76fa92b7ed39e10e
김효주 <hyojukim.dev@gmail.com>
2024-03-14 15:58:42 +0900

    add

---
+++ sungchef_backend/sungchef/src/test/java/com/sungchef/sungchef/SungchefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungchef.sungchef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SungchefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
