efb46801690f1b6072dfc8ff2f801fb7acdd6930
조남현 <zetta_byte@naver.com>
2024-03-18 13:46:18 +0900

    [ADD] GridComponent branch add

--- sungchef_android/app/src/main/java/com/ssafy/sungchef/features/component/LazyGridComponent.kt
+++ sungchef_android/app/src/main/java/com/ssafy/sungchef/features/component/LazyGridComponent.kt
@@ -1,3 +1,13 @@
 package com.ssafy.sungchef.features.component
 
- //.
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LazyVerticalGridDemo(){
+}
