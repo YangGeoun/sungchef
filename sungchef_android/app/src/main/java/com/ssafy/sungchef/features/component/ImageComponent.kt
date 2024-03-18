2617db11b004542e6d156ed5871520336f95145c
이우건 <bome519@naver.com>
2024-03-18 17:08:22 +0900

    Feat : 회원가입 남성, 여성 버튼 component 구현

--- sungchef_android/app/src/main/java/com/ssafy/sungchef/features/component/ImageComponent.kt
+++ sungchef_android/app/src/main/java/com/ssafy/sungchef/features/component/ImageComponent.kt
@@ -29,7 +29,7 @@ fun ImageComponent(
             .aspectRatio(1f),
         contentDescription = contentDescription,
         contentScale = ContentScale.FillBounds,
-        loading = placeholder(painterResource(id = R.drawable.icon_image_fail)),
-        failure = placeholder(painterResource(id = R.drawable.icon_image_fail))
+//        loading = placeholder(painterResource(id = R.drawable.icon_image_fail)),
+//        failure = placeholder(painterResource(id = R.drawable.icon_image_fail))
     )
 }
