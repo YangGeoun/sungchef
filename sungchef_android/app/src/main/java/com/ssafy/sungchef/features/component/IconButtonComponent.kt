a34343f9a087fbfcd142f01dd5e326b3f692322c
조남현 <zetta_byte@naver.com>
2024-03-18 13:12:58 +0900

    [ADD] Icon Component Add

---
+++ sungchef_android/app/src/main/java/com/ssafy/sungchef/features/component/IconButtonComponent.kt
@@ -0,0 +1,39 @@
+package com.ssafy.sungchef.features.component
+
+import android.util.Log
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun IconButtonComponent(
+    size : Int = 24,
+    onClick : () -> Unit,
+    colors: IconButtonColors = IconButtonDefaults.iconButtonColors(),
+    painter: Painter,
+
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = Modifier.size(size.dp),
+        colors = colors
+    ){
+        IconComponent(painter = painter, size = size)
+    }
+}
+
+
+@Preview
+@Composable
+private fun BodyPreview() {
+    IconButtonComponent(size = 24, onClick = { Log.d("TAG", "BodyPreview: ")}, painter = painterResource(id = androidx.core.R.drawable.ic_call_answer))
+}
---
+++ sungchef_android/app/src/main/java/com/ssafy/sungchef/features/component/IconComponent.kt
@@ -0,0 +1,32 @@
+package com.ssafy.sungchef.features.component
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun IconComponent(
+    size : Int = 24,
+    painter: Painter,
+    contentDescription : String = "contentDescription",
+    tint : Color = Color.Black
+) {
+    Icon(
+        modifier = Modifier.size(size.dp),
+        painter = painter,
+        contentDescription = contentDescription,
+        tint = tint)
+}
+
+
+@Preview
+@Composable
+private fun BodyPreview() {
+    IconComponent(size = 24, painter = painterResource(id = androidx.core.R.drawable.ic_call_answer))
+}
