2785007ac91e87c86fd1e26401f31ec4ec70255e
김성훈 <ksh7412.dev@gmail.com>
2024-03-20 16:45:01 +0900

    fix : 빌드 오류 수정

--- sungchef_backend/sungchef_dummy/src/main/java/com/sungchef/sungchef/ingredientservice/dto/response/ConvertProduct.java
+++ sungchef_backend/sungchef_dummy/src/main/java/com/sungchef/sungchef/ingredientservice/dto/response/ConvertProduct.java
@@ -1,10 +1,8 @@
 package com.sungchef.sungchef.ingredientservice.dto.response;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class ConvertProduct {
 	boolean isConverted;
 	String convertedName;
