33136dc976be349e572c8d7b4059e76d88f1da00
안성준 <hiahn777@gmail.com>
2024-03-22 01:00:39 +0900

    feat: 즐겨찾기순 조회 기능 개발

---
+++ sungchef_android/app/src/main/java/com/ssafy/sungchef/domain/usecase/recipe/GetBookMarkRecipeUseCase.kt
@@ -0,0 +1,15 @@
+package com.ssafy.sungchef.domain.usecase.recipe
+
+import com.ssafy.sungchef.commons.DataState
+import com.ssafy.sungchef.domain.model.recipe.RecipeInfo
+import com.ssafy.sungchef.domain.repository.RecipeRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetBookMarkRecipeUseCase @Inject constructor(
+    private val recipeRepository: RecipeRepository
+) {
+    suspend operator fun invoke(page: Int): Flow<DataState<List<RecipeInfo>>> {
+        return recipeRepository.getBookMarkRecipe(page)
+    }
+}
