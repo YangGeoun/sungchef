900fb001120c8fb2e9cb26c4d4e4adef9b31bcc3
김성훈 <ksh7412.dev@gmail.com>
2024-03-21 09:31:25 +0900

    feat : 유저 추천 리스트 변경 & 변경사항 대응

--- sungchef_backend/sungchef_dummy/src/main/java/com/sungchef/sungchef/recommendservice/controller/RecommendController.java
+++ sungchef_backend/sungchef_dummy/src/main/java/com/sungchef/sungchef/recommendservice/controller/RecommendController.java
@@ -38,27 +38,17 @@ public class RecommendController {
 			List<Recipe> recipeList = recommendRecipe.getRecipeList();
 
 			switch (recommendRecipe.getRecommendRecipeType()) {
-
-				case SUNG -> {
-					recipeList.add(
-						Recipe.builder()
-							.recipeId(10)
-							.recipeImage(
-								"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
-							.recipeName("국가권력급 김치찌개")
-							.build()
-					);
-				}
-
 				case FRIDGE -> {
-					recipeList.add(
-						Recipe.builder()
-							.recipeId(11)
-							.recipeImage(
-								"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
-							.recipeName("국가권력급 부대찌개")
-							.build()
-					);
+					for (int i = 0; i < 10; i++) {
+						recipeList.add(
+							Recipe.builder()
+								.recipeId(11 + i)
+								.recipeImage(
+									"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
+								.recipeName("국가권력급 부대찌개" + i)
+								.build()
+						);
+					}
 				}
 
 				default -> {
@@ -73,42 +63,62 @@ public class RecommendController {
 			List<Food> foodList = recommendFood.getFoodList();
 
 			switch (recommendFood.getRecommendFoodType()) {
+
+				case SUNG -> {
+					for (int i = 0; i < 10; i++) {
+						foodList.add(
+							Food.builder()
+								.foodImage(
+									"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
+								.foodName("돼지고기김치찌개" + i)
+								.build()
+						);
+					}
+				}
 				case AGE -> {
-					foodList.add(
-						Food.builder()
-							.foodImage(
-								"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
-							.foodName("김치찌개")
-							.build()
-					);
+					for (int i = 0; i < 10; i++) {
+						foodList.add(
+							Food.builder()
+								.foodImage(
+									"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
+								.foodName("김치찌개" + i)
+								.build()
+						);
+					}
 				}
 				case GENDER -> {
-					foodList.add(
-						Food.builder()
-							.foodImage(
-								"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
-							.foodName("부대찌개")
-							.build()
-					);
+					for (int i = 0; i < 10; i++) {
+						foodList.add(
+							Food.builder()
+								.foodImage(
+									"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
+								.foodName("부대찌개" + i)
+								.build()
+						);
+					}
 				}
 				case WEATHER -> {
-					foodList.add(
-						Food.builder()
-							.foodImage(
-								"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
-							.foodName("김치전")
-							.build()
-					);
+					for (int i = 0; i < 10; i++) {
+						foodList.add(
+							Food.builder()
+								.foodImage(
+									"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
+								.foodName("김치전" + i)
+								.build()
+						);
+					}
 				}
 
 				case SIMILAR_USER -> {
-					foodList.add(
-						Food.builder()
-							.foodImage(
-								"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
-							.foodName("오이김치")
-							.build()
-					);
+					for (int i = 0; i < 10; i++) {
+						foodList.add(
+							Food.builder()
+								.foodImage(
+									"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
+								.foodName("오이김치" + i)
+								.build()
+						);
+					}
 				}
 
 				default -> {
@@ -135,42 +145,61 @@ public class RecommendController {
 			List<Food> foodList = recommendFood.getFoodList();
 
 			switch (recommendFood.getRecommendFoodType()) {
+				case SUNG -> {
+					for (int i = 0; i < 10; i++) {
+						foodList.add(
+							Food.builder()
+								.foodImage(
+									"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
+								.foodName("돼지고기김치찌개" + i)
+								.build()
+						);
+					}
+				}
 				case AGE -> {
-					foodList.add(
-						Food.builder()
-							.foodImage(
-								"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
-							.foodName("김치찌개")
-							.build()
-					);
+					for (int i = 0; i < 10; i++) {
+						foodList.add(
+							Food.builder()
+								.foodImage(
+									"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
+								.foodName("김치찌개" + i)
+								.build()
+						);
+					}
 				}
 				case GENDER -> {
-					foodList.add(
-						Food.builder()
-							.foodImage(
-								"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
-							.foodName("부대찌개")
-							.build()
-					);
+					for (int i = 0; i < 10; i++) {
+						foodList.add(
+							Food.builder()
+								.foodImage(
+									"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
+								.foodName("부대찌개" + i)
+								.build()
+						);
+					}
 				}
 				case WEATHER -> {
-					foodList.add(
-						Food.builder()
-							.foodImage(
-								"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
-							.foodName("김치전")
-							.build()
-					);
+					for (int i = 0; i < 10; i++) {
+						foodList.add(
+							Food.builder()
+								.foodImage(
+									"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
+								.foodName("김치전" + i)
+								.build()
+						);
+					}
 				}
 
 				case SIMILAR_USER -> {
-					foodList.add(
-						Food.builder()
-							.foodImage(
-								"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
-							.foodName("오이김치")
-							.build()
-					);
+					for (int i = 0; i < 10; i++) {
+						foodList.add(
+							Food.builder()
+								.foodImage(
+									"https://static.wtable.co.kr/image-resize/production/service/recipe/291/4x3/a2421dff-e56c-40bd-8b40-06a91fc000a9.jpg")
+								.foodName("오이김치" + i)
+								.build()
+						);
+					}
 				}
 
 				default -> {
