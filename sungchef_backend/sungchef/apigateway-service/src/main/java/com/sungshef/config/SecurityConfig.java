4f8dff85805b00983f52b709fc57f4314c87878d
김성훈 <ksh7412.dev@gmail.com>
2024-03-23 04:57:38 +0900

    fix: 수정중

--- sungchef_backend/sungchef/apigateway-service/src/main/java/com/sungshef/config/SecurityConfig.java
+++ sungchef_backend/sungchef/apigateway-service/src/main/java/com/sungshef/config/SecurityConfig.java
@@ -2,20 +2,49 @@ package com.sungshef.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import lombok.AllArgsConstructor;
 
-@Configuration
+// @Configuration
 @EnableWebSecurity
-@AllArgsConstructor
 public class SecurityConfig {
 	@Bean
 	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		http.csrf(AbstractHttpConfigurer::disable);
+		// Configure AuthenticationManagerBuilder
+		AuthenticationManagerBuilder authenticationManagerBuilder =
+			http.getSharedObject(AuthenticationManagerBuilder.class);
+
+		AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+		http.csrf( (csrf) -> csrf.disable());
+		//        http.csrf(AbstractHttpConfigurer::disable);
+
+		http.authorizeHttpRequests((authz) -> authz
+				// .requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll()
+				// .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+				// .requestMatchers(new AntPathRequestMatcher("/users", "POST")).permitAll()
+				// .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+				//                        .requestMatchers("/**").access(this::hasIpAddress)
+				.requestMatchers("/**").permitAll()
+				.anyRequest().authenticated()
+			)
+			.authenticationManager(authenticationManager)
+			.sessionManagement((session) -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		// http.addFilter(getAuthenticationFilter(authenticationManager));
+		http.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.sameOrigin()));
+
 		return http.build();
 	}
 }
