245ba46582d07bd816859f69b4cd14dd6b6d1126
김성훈 <ksh7412.dev@gmail.com>
2024-03-25 17:57:01 +0900

    Revert "Revert "feat: 유저 로그인 요청 json 유효성 체크 추가""
    This reverts commit 8991bab06eafc7c2f18dc3742ba73a19db502293.

--- sungchef_backend/sungchef/user-service/src/main/java/com/ssafy/userservice/service/client/FridgeServiceClient.java
+++ sungchef_backend/sungchef/user-service/src/main/java/com/ssafy/userservice/service/client/FridgeServiceClient.java
@@ -1,11 +1,10 @@
-package com.ssafy.userservice.client;
+package com.ssafy.userservice.service.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.ssafy.userservice.util.result.CommonResult;
 import com.ssafy.userservice.util.result.SingleResult;
 
 @FeignClient("fridge-service")
