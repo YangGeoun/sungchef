f944328302e0671a0711ec161df727603875def5
안성준 <hiahn777@gmail.com>
2024-03-25 14:57:48 +0900

    hotfix: BaseDataSource 수정

--- sungchef_android/app/src/main/java/com/ssafy/sungchef/data/datasource/BaseRemoteDataSource.kt
+++ sungchef_android/app/src/main/java/com/ssafy/sungchef/data/datasource/BaseRemoteDataSource.kt
@@ -8,32 +8,34 @@ import com.ssafy.sungchef.commons.DataState
 import retrofit2.Response
 
 private const val TAG = "BaseRemoteDataSource_성식당"
+
 open class BaseRemoteDataSource {
     protected suspend fun <T> getResult(call: suspend () -> Response<T>): DataState<T> {
         val response = call()
-        return if (response.isSuccessful) {
-            val body = response.body()
-            if (body != null) DataState.Success(body)
-            else {
-                val apiError: APIError =
-                    Gson().fromJson(response.errorBody()?.charStream(), APIError::class.java)
-                DataState.Error(apiError)
-            }
-        } else {
-            when (response.code()) {
-                409 -> {
-                    val apiError = APIError(
-                        409L,
-                        ALREADY_NICKNAME
-                    )
-                    DataState.Error(apiError)
+        return try {
+            if (response.isSuccessful) {
+                val body = response.body()
+                if (body != null) DataState.Success(body)
+                else {
+                    DataState.Error(APIError(response.code().toLong(), "오류가 발생했습니다."))
                 }
-                else -> {
-                    val apiError: APIError =
-                        Gson().fromJson(response.errorBody()?.charStream(), APIError::class.java)
-                    DataState.Error(apiError)
+            } else {
+                when (response.code()) {
+                    409 -> {
+                        val apiError = APIError(
+                            409L,
+                            ALREADY_NICKNAME
+                        )
+                        DataState.Error(apiError)
+                    }
+
+                    else -> {
+                        DataState.Error(APIError(response.code().toLong(), "오류가 발생했습니다."))
+                    }
                 }
             }
+        } catch (e: Exception) {
+            DataState.Error(APIError(-1, "오류가 발생했습니다."))
         }
     }
 }
