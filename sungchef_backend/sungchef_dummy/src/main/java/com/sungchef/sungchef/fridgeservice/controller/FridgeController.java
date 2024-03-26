4c22852fa36c50595f600acb97d0d197bd350238
김성훈 <ksh7412.dev@gmail.com>
2024-03-26 10:29:12 +0900

    refactor : 부족한 재료 확인 api 변경
    fridge/need/{recipeId} -> ingredient/need/{recipeId}

--- sungchef_backend/sungchef_dummy/src/main/java/com/sungchef/sungchef/fridgeservice/controller/FridgeController.java
+++ sungchef_backend/sungchef_dummy/src/main/java/com/sungchef/sungchef/fridgeservice/controller/FridgeController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +17,6 @@ import com.sungchef.sungchef.fridgeservice.dto.response.Ingredient;
 import com.sungchef.sungchef.fridgeservice.dto.response.IngredientInfo;
 import com.sungchef.sungchef.fridgeservice.service.SampleService;
 import com.sungchef.sungchef.util.exception.IngredientNotFoundException;
-import com.sungchef.sungchef.util.exception.RecipeNotFoundException;
 import com.sungchef.sungchef.util.responsehelper.ResponseService;
 
 import lombok.RequiredArgsConstructor;
@@ -158,111 +156,6 @@ public class FridgeController {
 				responseService.getSuccessMessageResult("재료 등록 성공")
 			);
 		} catch (IngredientNotFoundException e) {
-			return responseService.BAD_REQUEST();
-		} catch (Exception e) {
-			return responseService.INTERNAL_SERVER_ERROR();
-		}
-	}
-
-	@GetMapping("/need/{recipeId}")
-	public ResponseEntity<?> getIngredientIdToCook(@PathVariable("recipeId") final String recipeId) {
-		try {
-			log.debug("/need/{recipeId} : {}", recipeId);
-			FridgeIngredientListRes needIngredientListRes = new FridgeIngredientListRes();
-
-			List<IngredientInfo> ingredientInfoList = needIngredientListRes.getIngredientInfoList();
-
-			for (IngredientInfo info : ingredientInfoList) {
-
-				List<Ingredient> ingredientList = info.getIngredientList();
-
-				switch (info.getIngredientType()) {
-
-					case FRUIT -> {
-						log.debug("과일 : {}", info.getIngredientType().name());
-						ingredientList.add(
-							Ingredient.builder()
-								.ingredientId(10)
-								.ingredientName("사과")
-								.build()
-						);
-					}
-					case VEGETABLE -> {
-						log.debug("채소 : {}", info.getIngredientType().name());
-						ingredientList.add(
-							Ingredient.builder()
-								.ingredientId(11)
-								.ingredientName("대파")
-								.build()
-						);
-					}
-					case RICE_GRAIN -> {
-						log.debug("쌀/곡물 : {}", info.getIngredientType().name());
-						ingredientList.add(
-							Ingredient.builder()
-								.ingredientId(12)
-								.ingredientName("미숫가루")
-								.build()
-						);
-					}
-					case MEAT_EGG -> {
-						log.debug("정육/계란 : {}", info.getIngredientType().name());
-						ingredientList.add(
-							Ingredient.builder()
-								.ingredientId(13)
-								.ingredientName("삼겹살")
-								.build()
-						);
-					}
-					case FISH -> {
-						log.debug("수산 : {}", info.getIngredientType().name());
-						ingredientList.add(
-							Ingredient.builder()
-								.ingredientId(21)
-								.ingredientName("고등어")
-								.build()
-						);
-					}
-					case MILK -> {
-						log.debug("유제품 : {}", info.getIngredientType().name());
-						ingredientList.add(
-							Ingredient.builder()
-								.ingredientId(120)
-								.ingredientName("블루치즈")
-								.build()
-						);
-					}
-					case SAUCE -> {
-						log.debug("소스/양념/조미료 : {}", info.getIngredientType().name());
-						ingredientList.add(
-							Ingredient.builder()
-								.ingredientId(30)
-								.ingredientName("치킨스톡")
-								.build()
-						);
-					}
-					case ETC -> {
-						log.debug("기타 : {}", info.getIngredientType().name());
-						ingredientList.add(
-							Ingredient.builder()
-								.ingredientId(500)
-								.ingredientName("먹다 남은 치킨")
-								.build()
-						);
-					}
-
-					default -> {
-						return responseService.INTERNAL_SERVER_ERROR();
-					}
-
-				}
-			}
-			return ResponseEntity.ok(
-				responseService.getSuccessSingleResult(
-					needIngredientListRes, "부족한 재료 목록 조회 성공"
-				)
-			);
-		} catch (RecipeNotFoundException e) {
 			return responseService.BAD_REQUEST();
 		} catch (Exception e) {
 			return responseService.INTERNAL_SERVER_ERROR();
