1c26ac375eb6577b7173446af9ab406a278033cd
이우건 <bome519@naver.com>
2024-03-28 23:09:55 +0900

    Update : 임시 커밋

--- sungchef_android/app/src/main/java/com/ssafy/sungchef/features/screen/MainActivity.kt
+++ sungchef_android/app/src/main/java/com/ssafy/sungchef/features/screen/MainActivity.kt
@@ -15,6 +15,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.collectAsState
 import androidx.datastore.preferences.preferencesDataStore
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.NidOAuthLogin
+import com.navercorp.nid.oauth.OAuthLoginCallback
 import com.ssafy.sungchef.commons.CAMERA_PERMISSON_DENIED
 import com.ssafy.sungchef.commons.CAMERA_PERMISSON_GRANTED
 import com.ssafy.sungchef.features.navigation.NavGraph
@@ -38,6 +41,8 @@ class MainActivity : ComponentActivity(), PermissionResultListener {
         }
 
         permissionChecker.requestCameraPermission()
+//        naverUnlink()
+
         // TODO 자동 로그인 로직 필요
         setContent {
             SungchefTheme {
@@ -64,5 +69,30 @@ class MainActivity : ComponentActivity(), PermissionResultListener {
     override fun onPermissionDenied() {
         Toast.makeText(this, CAMERA_PERMISSON_DENIED, Toast.LENGTH_SHORT).show()
         finish()
+    }
+
+    // 네이버 로그아웃
+    private fun naverLogout(){
+        NaverIdLoginSDK.logout()
+    }
+
+    // 네이버 연결끊기
+    private fun naverUnlink(){
+        NidOAuthLogin().callDeleteTokenApi(object : OAuthLoginCallback {
+            override fun onSuccess() {
+                //서버에서 토큰 삭제에 성공한 상태입니다.
+            }
+            override fun onFailure(httpStatus: Int, message: String) {
+                // 서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+                // 클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+                Log.d("naver", "errorCode: ${NaverIdLoginSDK.getLastErrorCode().code}")
+                Log.d("naver", "errorDesc: ${NaverIdLoginSDK.getLastErrorDescription()}")
+            }
+            override fun onError(errorCode: Int, message: String) {
+                // 서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+                // 클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+                onFailure(errorCode, message)
+            }
+        })
     }
 }
