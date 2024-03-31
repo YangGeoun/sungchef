e5b01ea320ec505f6fe6ec82654d717406aab1e9
김효주 <>
2024-04-01 00:16:00 +0900

    fix: ingredientServiceClient parameter 에 @RequestBody @RequestHeader 분리해서 추가

--- sungchef_backend/sungchef/fridge-service/src/main/java/com/ssafy/fridgeservice/service/client/IngredientServiceClient.java
+++ sungchef_backend/sungchef/fridge-service/src/main/java/com/ssafy/fridgeservice/service/client/IngredientServiceClient.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import com.ssafy.fridgeservice.dto.request.user.SignUpReq;
 import com.ssafy.fridgeservice.util.result.SingleResult;
@@ -18,7 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 public interface IngredientServiceClient {
 
 	@PostMapping("/ingredient/list")
-	ResponseEntity<?> getIngredientInfoList(List<Integer> ingredientIdList, String token);
+	ResponseEntity<?> getIngredientInfoList(@RequestBody List<Integer> ingredientIdList, @RequestHeader("Authorization")String token);
 
 	@GetMapping("/ingredient/communication")
 	String communicationTest(String token);
