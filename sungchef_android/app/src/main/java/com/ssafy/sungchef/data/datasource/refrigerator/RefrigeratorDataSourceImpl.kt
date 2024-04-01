e892ae541ed99ea84c8f0a6cf1cbdb24aaa28ecd
이우건 <bome519@naver.com>
2024-04-01 13:56:43 +0900

    Update: model refrigerator 패키지, 냉장고 관련 여러 설정들 추가

--- sungchef_android/app/src/main/java/com/ssafy/sungchef/data/datasource/refrigerator/RefrigeratorDataSourceImpl.kt
+++ sungchef_android/app/src/main/java/com/ssafy/sungchef/data/datasource/refrigerator/RefrigeratorDataSourceImpl.kt
@@ -1,10 +1,18 @@
 package com.ssafy.sungchef.data.datasource.refrigerator
 
+import com.ssafy.sungchef.commons.DataState
 import com.ssafy.sungchef.data.api.RefrigeratorService
+import com.ssafy.sungchef.data.datasource.BaseRemoteDataSource
+import com.ssafy.sungchef.data.model.responsedto.ResponseDto
+import com.ssafy.sungchef.data.model.responsedto.ingredient.search.SearchIngredientResponse
 import javax.inject.Inject
 
 class RefrigeratorDataSourceImpl @Inject constructor(
     private val refrigeratorService: RefrigeratorService
-) : RefrigeratorDataSource {
-
+) : RefrigeratorDataSource, BaseRemoteDataSource() {
+    override suspend fun searchIngredient(ingredientName: String): DataState<ResponseDto<SearchIngredientResponse>> {
+        return getResult {
+            refrigeratorService.searchIngredient(ingredientName)
+        }
+    }
 }
--- sungchef_android/app/src/main/java/com/ssafy/sungchef/domain/repository/RefrigeratorRepository.kt
+++ sungchef_android/app/src/main/java/com/ssafy/sungchef/domain/repository/RefrigeratorRepository.kt
@@ -1,4 +1,11 @@
 package com.ssafy.sungchef.domain.repository
 
+import com.ssafy.sungchef.commons.DataState
+import com.ssafy.sungchef.data.model.responsedto.ResponseDto
+import com.ssafy.sungchef.data.model.responsedto.ingredient.search.SearchIngredientResponse
+import kotlinx.coroutines.flow.Flow
+
 interface RefrigeratorRepository {
+
+    suspend fun searchIngredient(ingredientName : String) : Flow<DataState<ResponseDto<SearchIngredientResponse>>>
 }
