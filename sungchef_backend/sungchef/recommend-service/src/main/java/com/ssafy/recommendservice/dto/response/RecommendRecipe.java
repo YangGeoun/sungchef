1b774888d23a4b3581816c786af27e0e9599e877
양건우 <mywe2365@gmail.com>
2024-04-02 12:18:34 +0900

    [feat] 추천 로직 완전 적용

--- sungchef_backend/sungchef/recommend-service/src/main/java/com/ssafy/recommendservice/dto/response/RecommendRecipe.java
+++ sungchef_backend/sungchef/recommend-service/src/main/java/com/ssafy/recommendservice/dto/response/RecommendRecipe.java
@@ -1,18 +1,20 @@
 package com.ssafy.recommendservice.dto.response;
 
 import com.ssafy.recommendservice.util.sungchefEnum.RecommendRecipeType;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@Builder
 public class RecommendRecipe {
-	RecommendRecipeType recommendRecipeType;
+	String recommendRecipeType;
 	List<Recipe> recipeList;
 
-	public RecommendRecipe(RecommendRecipeType _recommendRecipeType) {
-		recommendRecipeType = _recommendRecipeType;
-		recipeList = new ArrayList<>();
-	}
+//	public RecommendRecipe(RecommendRecipeType _recommendRecipeType) {
+//		recommendRecipeType = _recommendRecipeType;
+//		recipeList = new ArrayList<>();
+//	}
 }
